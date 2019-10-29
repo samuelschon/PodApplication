54bda7af764253a2df219a65c286d8b7a04930d9
Samuel Schön <Samuel.Schon@hotmail.com>
2019-10-29 09:18:46 +0100

    Added Category class

---
+++ Gui/SharedModels/Models/Category.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedModels.Models
+{
+   public class Category
+    {
+        public string Name { get; set; }
+        
+
+        public Category(string name)
+        {
+            Name = name;
+
+
+        }
+
+
+
+
+
+
+    }
+}
