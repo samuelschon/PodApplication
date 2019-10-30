7428b12a430c5844f9d2d9dc33ef4cb266a1f628
Aryan <meerajmm7@gmail.com>
2019-10-30 11:54:14 +0100

    Added TimerService

---
+++ Gui/Logic/Timers/TimerService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using SharedModels.Models;
+
+namespace Logic.Timers
+{
+    public static class TimerService
+    {
+        
+        public static void StartTimer(Feed feed)
+        {
+            
+            Timer updateTimer = new Timer();
+            updateTimer.Elapsed += TimerElapsedHandler;
+            updateTimer.Interval = 1000;
+            updateTimer.Enabled = true;
+            updateTimer.AutoReset = true;
+
+        }
+
+        public static void TimerElapsedHandler(object sender, ElapsedEventArgs e)
+        {
+            System.Windows.Forms.MessageBox.Show("Nu fungerar detta..");
+        }
+
+
+    }
+    
+}
